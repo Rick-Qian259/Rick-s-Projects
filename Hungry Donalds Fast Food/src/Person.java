ef006153f3674a89f1c79464b0bbc36588bdd242
Rick-Qian259 <83938514+Rick-Qian259@users.noreply.github.com>
2021-08-14 12:03:18 +0930

    The project source codes
    Reformatall the source codes.

---
+++ Hungry Donalds Fast Food/src/Person.java
@@ -0,0 +1,40 @@
+/*
+ *   Foundations of Computer Science
+ *   2018, Semester 01
+ *   Practical-Exam-04
+ *
+ *   student (id): a111111
+ *   student (name): John Smitth
+ *
+ *
+ * Note: your are required to perform small changes, as accessors and mutators;
+ *
+ */
+public abstract class Person {
+    protected String name;
+    protected int age;
+
+
+    Person() {
+        this.name = "unnamed";
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /* implement accessors and mutators */
+}
